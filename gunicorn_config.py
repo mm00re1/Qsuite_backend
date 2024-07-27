d17ed2336f4c5b18a91107f79053b7aed4c0dd9b
m00re1 <mauricemoore100@gmail.com>
2024-07-27 11:08:37 +0200

    preparing foir running on WSL or linux server

---
+++ gunicorn_config.py
@@ -0,0 +1,4 @@
+bind = "0.0.0.0:8000"
+workers = 4  # Number of worker processes
+worker_class = "uvicorn.workers.UvicornWorker"
+loglevel = "info"
--- main.py
+++ main.py
@@ -29,7 +29,3 @@ app.include_router(add_view_test_groups.router)
 app.include_router(search_tests.router)
 app.include_router(view_tests.router)
 app.include_router(run_q_code.router)
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
